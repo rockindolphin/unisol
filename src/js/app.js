3aa76c12ee174d7946edbd66f71f3eacc999b506
rockindolphin <rockindolphing@gmail.com>
2018-07-16 10:16:39 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+(function($){
+	$(document).ready(function() {
+
+		//ширина скроллбара
+		var scrollMeasure = $('<div>').addClass('scroll__measure').get(0);
+		$('body').append(scrollMeasure);
+		var scrollbarWidth = scrollMeasure.offsetWidth - scrollMeasure.clientWidth;
+		$(':root').css('--scrollbar-width', scrollbarWidth+'px');
+		if( scrollbarWidth > 0 ){
+			$('.scroll--cutt').css({
+				marginRight: -scrollbarWidth
+			});		
+		}
+		
+		//bg
+		$('img[data-bg=true]').each(function(){
+			var src = $(this).attr('src');
+			var parent = $(this).parent();
+			if( $(parent).is('picture') ){
+				src = $(parent).find('img').get(0).currentSrc || $(parent).find('img').get(0).src;
+				parent =  $(parent).parent();
+			}
+			$(parent).css({
+				'background-image': `url(${src})`
+			});
+			$(this).hide();
+		});
+
+		
+
+	});	
+
+})(jQuery);
