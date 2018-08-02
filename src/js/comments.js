9e24e18c691fea7435673e0bac5005bdd6626274
Unknown <rockindolphing@gmail.com>
2018-08-02 16:18:14 +0300

    fix

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,11 +1,15 @@
-if( document.querySelector('#mc-container') ){
-	var cackle_widget = window.cackle_widget || [];
-	cackle_widget.push({widget: 'Comment', id: 60951});
-	(function() {
-		var mc = document.createElement('script');
-		mc.type = 'text/javascript';
-		mc.async = true;
-		mc.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cackle.me/widget.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
-	})();
-}
+document.addEventListener('DOMContentLoaded', function(){
+	
+	if( document.querySelector('#mc-container') ){
+		window.cackle_widget = window.cackle_widget || [];
+		cackle_widget.push({widget: 'Comment', id: 60951});
+		(function() {
+			var mc = document.createElement('script');
+			mc.type = 'text/javascript';
+			mc.async = true;
+			mc.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cackle.me/widget.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+		})();
+	}
+	
+});
